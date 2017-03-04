1727c73ce4826f4b2e9c529fbec1c4461ff7c51d
Massimiliano Angelino <massi.ang@gmail.com>
2017-03-04 15:14:00 +0100

    TypeScript source

--- multipart.js
+++ multipart.js
@@ -1,159 +1,136 @@
 #! /usr/bin/env node 
-
-const program = require('commander')
-const aws=require('aws-sdk')
-const cp = require('child_process')
-const os = require('os')
-const fs=require('fs')
-const url = require('url')
-const path = require('path')
-const filesize = require('filesize')
-//const ProgressBar = require('ascii-progress');
-const ProgressBar = require('progress')
-var ncpus = os.cpus().length
-
-var s3 = new aws.S3()
-
+"use strict";
+exports.__esModule = true;
+var program = require("commander");
+var fs = require("fs");
+var url = require("url");
+var filesize = require("filesize");
+//import * as ProgressBar from 'ascii-progress';;
+var ProgressBar = require("progress");
+var aws_sdk_1 = require("aws-sdk");
+var s3 = new aws_sdk_1.S3();
 function makeCounter(limit, callback) {
-	return function() {
-		if (--limit === 0) {
-			callback();
-		}
-	}
+    return function () {
+        if (--limit === 0) {
+            callback();
+        }
+    };
 }
-
 function download(params, chunks, fd) {
-	var start_time = process.hrtime();
-	// progress is not a top library, would have preferred ansi-progress but 
-	// does not work in ssh remote terminals
-	var bar = new ProgressBar('  [:bar] :percent :etas', {
-			complete: '=',
-			incomplete: ' ',
-			width: 60,
-			total: chunks.upper-chunks.lower
-		});
-	bar.tick(0);
-
-	var fetchers_count=Math.ceil((chunks.upper-chunks.lower)/chunks.size);
-
-	var file_done = makeCounter(fetchers_count, function() { 
-		fs.closeSync(fd);
-		var end_time = process.hrtime(start_time); 
-		bar.terminate();
-		console.log(`Download completed in ${end_time}s at ${chunks.upper/1024./1024/end_time[0]} Mibps`)
-		
-	});
-
-	var task = function(idx, args) {
-		//The idx indicates the chunk to download from the params base
-		var lower = args.lower+idx*args.size;
-		var upper = Math.min(lower+args.size-1, args.upper);
-
-		params.Range = "bytes="+lower+"-"+upper;
-		var f = function() {
-			s3.getObject(this.p, 
-			function(err, data) { 
-				if (err) {
-					console.log(err.code); 
-					error=true;
-					process.exit(1);
-				} else { 
-					if (fd) {
-						fs.write(fd,data.Body, 0, data.Body.length, this.lower, (err, written, buffer) => {
-							if (err) {
-								throw err;
-							}
-							bar.tick(written);
-							file_done();
-						});
-					} else {
-						bar.tick(data.Body.length);
-					}
-				}
-			}.bind({lower:lower}));
-		}.bind({p:params});
-		f();
-	}
-	// Starts all async fetcher tasks
-	for (var k=0; k<fetchers_count; k++) {
-		task(k, chunks)
-	}
+    var start_time = process.hrtime();
+    // progress is not a top library, would have preferred ansi-progress but 
+    // does not work in ssh remote terminals
+    var bar = new ProgressBar('  [:bar] :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 60,
+        total: chunks.upper - chunks.lower
+    });
+    bar.tick(0);
+    var fetchers_count = Math.ceil((chunks.upper - chunks.lower) / chunks.size);
+    var file_done = makeCounter(fetchers_count, function () {
+        fs.closeSync(fd);
+        var end_time = process.hrtime(start_time);
+        bar.terminate();
+        console.log("Download completed in " + end_time + "s at " + chunks.upper / 1024. / 1024 / end_time[0] + " Mibps");
+    });
+    var task = function (idx, args) {
+        //The idx indicates the chunk to download from the params base
+        var lower = args.lower + idx * args.size;
+        var upper = Math.min(lower + args.size - 1, args.upper);
+        params.Range = "bytes=" + lower + "-" + upper;
+        var p = params;
+        s3.getObject(p, function (err, data) {
+            if (err) {
+                console.log(err.code);
+                process.exit(1);
+            }
+            else {
+                if (fd) {
+                    fs.write(fd, data.Body, 0, data.Body.length, lower, function (err, written, buffer) {
+                        if (err) {
+                            throw err;
+                        }
+                        bar.tick(written);
+                        file_done();
+                    });
+                }
+                else {
+                    bar.tick(data.Body.length);
+                }
+            }
+        });
+    };
+    // Starts all async fetcher tasks
+    for (var k = 0; k < fetchers_count; k++) {
+        task(k, chunks);
+    }
 }
-
-//TODO: Add access key and secret as optional parameters
 program
-	.version('1.0.0')
-	.usage('[options] <s3object> <outputfile>')
-	.arguments('<s3object> <outputfile>')
-	.option('-s, --size <size>', 'The size of the chunks to download', parseInt)
-	.option('-t, --test', 'Test mode - does not write to disk')
-	.option('-c, --credentials <credentials>', 'accessKey/secretAccessKey for the access to AWS S3')
-	.action((s3object, outputfile) => {
-		if (typeof s3object === 'undefined') {
-			console.error('An S3 URI must be provided');
-			process.exit(1);
-		}
-		var error_message;
-		//console.log('Getting file size'+file);
-		var s3uri = url.parse(s3object);
-		if (s3uri.protocol !== 's3:') {
-			error_message = `Not a valid S3 object ${s3object}`
-		}
-		if (program.credentials) {
-			var c = program.credentials.split('/');
-			if (c.length != 2) {
-				error_message = 'Invalid credential. Must be specified as <accessKey>/<secretAccessKey>'
-			} else {
-				var aws_cred = new aws.Credentials(c[0], c[1]);
-				//var config = new aws.Config();
-				//config.credentials = aws_cred;
-				aws.config.credentials = aws_cred;
-				s3 = new aws.S3()
-			}
-		}
-		if (error_message) {
-			console.error(error_message);
-			process.exit(1);
-		}
-		
-		// Paramter object for S3 client
-		var params = {
-			Bucket: s3uri.host,
-			Key: s3uri.path.substr(1)
-		}
-
-		s3.headObject(params, function(err, data) { 
-			if (err) {
-				console.log(`Error getting object from S3: ${err.code}`);
-				process.exit(1); 
-			}
-			else {
-				var size = parseInt(data.ContentLength);
-				console.log("Total size = %s", filesize(size));
-				var chunk_size = 2000000;
-				if (program.size) {
-					chunk_size = program.size;
-				}
-				var test = false;
-				if (program.test)
-					test = true;
-				
-				console.log(`Chunk size = ${filesize(chunk_size)}`);
-				var fd;
-				if (!test) {
-					fd = fs.openSync(outputfile, 'w');
-					fs.writeSync(fd,['\0'],0,1,size-1);
-				}			
-				chunks = {
-					'lower': 0, 
-					'upper': size,
-					'size': chunk_size
-				}
-				download(params, chunks, fd);				
-			}
-		});
-	});
-
+    .version('1.0.0')
+    .usage('[options] <s3object> <outputfile>')
+    .arguments('<s3object> <outputfile>')
+    .option('-s, --size <size>', 'The size of the chunks to download', parseInt)
+    .option('-t, --test', 'Test mode - does not write to disk')
+    .option('-c, --credentials <credentials>', 'accessKey/secretAccessKey for the access to AWS S3')
+    .action(function (s3object, outputfile) {
+    if (typeof s3object === 'undefined') {
+        console.error('An S3 URI must be provided');
+        process.exit(1);
+    }
+    var error_message;
+    var s3uri = url.parse(s3object);
+    if (s3uri.protocol !== 's3:') {
+        error_message = "Not a valid S3 object " + s3object;
+    }
+    if (program.credentials) {
+        var c = program.credentials.split('/');
+        if (c.length != 2) {
+            error_message = 'Invalid credential. Must be specified as <accessKey>/<secretAccessKey>';
+        }
+        else {
+            var aws_cred = new aws_sdk_1.Credentials(c[0], c[1]);
+            aws_sdk_1.config.credentials = aws_cred;
+            s3 = new aws_sdk_1.S3();
+        }
+    }
+    if (error_message) {
+        console.error(error_message);
+        process.exit(1);
+    }
+    // Paramter object for S3 client
+    var params = {
+        Bucket: s3uri.host,
+        Key: s3uri.path.substr(1)
+    };
+    s3.headObject(params, function (err, data) {
+        if (err) {
+            console.log("Error getting object from S3: " + err.code);
+            process.exit(1);
+        }
+        else {
+            var size = data.ContentLength;
+            console.log("Total size = %s", filesize(size));
+            var chunk_size = 2000000;
+            if (program.size) {
+                chunk_size = program.size;
+            }
+            var test = false;
+            if (program.test)
+                test = true;
+            console.log("Chunk size = " + filesize(chunk_size));
+            var fd = void 0;
+            if (!test) {
+                fd = fs.openSync(outputfile, 'w');
+                fs.writeSync(fd, new Buffer(['\0']), 0, 1, size - 1);
+            }
+            var chunks = {
+                'lower': 0,
+                'upper': size,
+                'size': chunk_size
+            };
+            download(params, chunks, fd);
+        }
+    });
+});
 program.parse(process.argv);
-
-
---
+++ multipart.ts
@@ -0,0 +1,152 @@
+#! /usr/bin/env node 
+
+import * as program from 'commander';
+import * as cp from 'child_process';
+import * as os from 'os';
+import * as fs from 'fs';
+import * as url from 'url';
+import * as path from 'path';
+import * as filesize from 'filesize';
+//import * as ProgressBar from 'ascii-progress';;
+import * as ProgressBar from 'progress';
+import { AWSError, S3, Config, Credentials, config } from "aws-sdk";
+
+let s3 = new S3()
+
+function makeCounter(limit: number, callback) {
+	return function() {
+		if (--limit === 0) {
+			callback();
+		}
+	}
+}
+
+function download(params, chunks, fd: number): void {
+	let start_time = process.hrtime();
+	// progress is not a top library, would have preferred ansi-progress but 
+	// does not work in ssh remote terminals
+	let bar = new ProgressBar('  [:bar] :percent :etas', {
+			complete: '=',
+			incomplete: ' ',
+			width: 60,
+			total: chunks.upper-chunks.lower
+		});
+	bar.tick(0);
+
+	let fetchers_count=Math.ceil((chunks.upper-chunks.lower)/chunks.size);
+
+	let file_done = makeCounter(fetchers_count, function() { 
+		fs.closeSync(fd);
+		let end_time = process.hrtime(start_time); 
+		bar.terminate();
+		console.log(`Download completed in ${end_time}s at ${chunks.upper/1024./1024/end_time[0]} Mibps`)
+		
+	});
+
+	let task = function(idx: number, args) {
+		//The idx indicates the chunk to download from the params base
+		let lower = args.lower+idx*args.size;
+		let upper = Math.min(lower+args.size-1, args.upper);
+
+		params.Range = "bytes="+lower+"-"+upper;
+		let p = params;
+
+		s3.getObject(p, 
+			function (err: AWSError, data) { 
+				if (err) {
+					console.log(err.code); 
+					process.exit(1);
+				} else { 
+					if (fd) {
+						fs.write(fd,data.Body as Buffer, 0, (data.Body as Buffer).length, lower, (err, written, buffer) => {
+							if (err) {
+								throw err;
+							}
+							bar.tick(written);
+							file_done();
+						});
+					} else {
+						bar.tick((data.Body as Buffer).length);
+					}
+				}
+			});
+	}
+	// Starts all async fetcher tasks
+	for (let k=0; k<fetchers_count; k++) {
+		task(k, chunks)
+	}
+}
+
+program
+	.version('1.0.0')
+	.usage('[options] <s3object> <outputfile>')
+	.arguments('<s3object> <outputfile>')
+	.option('-s, --size <size>', 'The size of the chunks to download', parseInt)
+	.option('-t, --test', 'Test mode - does not write to disk')
+	.option('-c, --credentials <credentials>', 'accessKey/secretAccessKey for the access to AWS S3')
+	.action((s3object: string, outputfile: string) => {
+		if (typeof s3object === 'undefined') {
+			console.error('An S3 URI must be provided');
+			process.exit(1);
+		}
+		let error_message: string;
+		let s3uri = url.parse(s3object);
+		if (s3uri.protocol !== 's3:') {
+			error_message = `Not a valid S3 object ${s3object}`
+		}
+		if (program.credentials) {
+			let c = program.credentials.split('/');
+			if (c.length != 2) {
+				error_message = 'Invalid credential. Must be specified as <accessKey>/<secretAccessKey>'
+			} else {
+				let aws_cred = new Credentials(c[0], c[1]);
+				config.credentials = aws_cred;
+				s3 = new S3()
+			}
+		}
+		if (error_message) {
+			console.error(error_message);
+			process.exit(1);
+		}
+		
+		// Paramter object for S3 client
+		let params = {
+			Bucket: s3uri.host,
+			Key: s3uri.path.substr(1)
+		}
+
+		s3.headObject(params, function(err, data) { 
+			if (err) {
+				console.log(`Error getting object from S3: ${err.code}`);
+				process.exit(1); 
+			}
+			else {
+				let size = data.ContentLength;
+				console.log("Total size = %s", filesize(size));
+				let chunk_size = 2000000;
+				if (program.size) {
+					chunk_size = program.size;
+				}
+				let test = false;
+				if (program.test)
+					test = true;
+				
+				console.log(`Chunk size = ${filesize(chunk_size)}`);
+				let fd;
+				if (!test) {
+					fd = fs.openSync(outputfile, 'w');
+					fs.writeSync(fd,new Buffer(['\0']),0,1,size-1);
+				}			
+				let chunks = {
+					'lower': 0, 
+					'upper': size,
+					'size': chunk_size
+				}
+				download(params, chunks, fd);				
+			}
+		});
+	});
+
+program.parse(process.argv);
+
+
